ebc4b07ababe5ff878737baee4f936024006ef34
xetz <enecnd1152@gmail.com>
2024-12-21 21:34:48 +0300

    kjskd

--- producer-consumer-exmp/main.c
+++ producer-consumer-exmp/main.c
@@ -1,16 +1,58 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<pthread.h>
-#include<unistd.h>
-#include<time.h>
-#include<semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <semaphore.h>
 
 #define THREAD_NUM 2
 
 int buffer[10];
 int count = 0;
 
-int main() {
-    printf("hello world\n");
+void *producer(void *args)
+{
+    //produce
+    int x = rand() % 100;
+
+    //add to the buffer
+    buffer[count] = x;
+    count++;
+}
+
+void *consumer(void *args)
+{
+    //remove from buffer
+    int y = buffer[count - 1];
+    count--;
+
+    //consume
+    printf("Got %d\n", y);
+}
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    pthread_t th[THREAD_NUM];
+    int i;
+    for (i = 0; i < THREAD_NUM; i++){
+        if (i % 2 == 0){
+            if (pthread_create(&th[i], NULL, &producer, NULL) != 0){
+                perror("Failed to create thread!\n");
+            }
+        else{
+            if (pthread_create(&th[i], NULL, &consumer, NULL) != 0){
+                perror("Failed to create thread!\n");
+            }
+        }
+        }
+    }
+
+    for(i = 0; i<THREAD_NUM; i++){
+        if(pthread_join(&th[i],NULL) != 0){
+            perror("Failed to join threads!\n");
+        }
+    }
+    return 0;
 }
